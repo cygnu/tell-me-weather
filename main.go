a50baebedfd2859f35bcac10442a355705e05523
cygnu <crucia8@icloud.com>
2021-07-22 18:21:37 +0900

    feat: Implement handler

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cygnu/tell-me-weather/config"
+	"github.com/cygnu/tell-me-weather/openweather"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
-func main()  {
+func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/", handler)
+
+	start := e.Start(fmt.Sprintf(":%v", config.Config.Port))
+	e.Logger.Fatal(start)
+}
+
+func handler(c echo.Context) error {
+	apiClient, _ := openweather.NewAPIClient(openweather.BaseURL, config.Config.ApiKey)
+	apiClient.GetForecast("Tokyo,jp")
+
+	return nil
 }
